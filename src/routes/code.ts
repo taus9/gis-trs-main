84612abee09e1d75a649e7e6688e73d273059449
Christopher Martinez <crmartinez239@protonmail.com>
2025-12-03 14:40:33 -0500

    Refactor code and token routes to trust error middleware

--- src/routes/code.ts
+++ src/routes/code.ts
@@ -7,29 +7,22 @@ import { ConfigShape } from "../shared/config.ts";
 
 export function createCodeHandler(_store: TokenStore, config: ConfigShape) {
     return async (ctx: Context) => {
-
-        try {
-            const payload = ctx.state.validatedBody as z.infer<typeof tokenRequestSchema>;
-            const response = await getTokenFromCode(payload.code, config);
-
-            if ("access_token" in response) {
-                const expires_at = Date.now() + (response.expires_in - config.REFRESH_MARGIN) * 1000;
-                const store: StoredToken = {...response, expires_at, user_id};
-                await _store.upsert(store);
-                console.log("New token from code successful: user_id:", user_id);
-                ctx.response.status = 200;
-                ctx.response.body = { message: "Token stored" };
-            } else {
-                console.error("Error getting token from code:", response.error_description);
-                ctx.response.status = response.status;
-                ctx.response.body = { error: response.error_description };
-            }
-
-        } catch (err) {
-            console.error("Route /code error:", err);
-            ctx.response.status = 500;
-            ctx.response.body = { error: "Internal server error" };
+        // the user_id was set in createKeyCheckMiddleware
+        const user_id = ctx.state.user_id as string;
+        // payload was validated in validateBody middleware
+        const payload = ctx.state.validatedBody as z.infer<typeof tokenRequestSchema>;
+        const response = await getTokenFromCode(payload.code, config);
+        if ("access_token" in response) {
+            const expires_at = Date.now() + (response.expires_in - config.REFRESH_MARGIN) * 1000;
+            const store: StoredToken = {...response, expires_at, user_id};
+            await _store.upsert(store);
+            console.log("New token from code successful: user_id:", user_id);
+            ctx.response.status = 200;
+            ctx.response.body = { message: "Token stored" };
+        } else {
+            console.error("Error getting token from code:", response.error_description);
+            ctx.response.status = response.status;
+            ctx.response.body = { error: response.error_description };
         }
-
     }
 }
--- src/routes/token.ts
+++ src/routes/token.ts
@@ -2,53 +2,46 @@ import { Context } from "@oak";
 import { TokenStore, StoredToken } from "../interfaces/token.ts";
 import { getTokenFromRefresh } from "../shared/goto.ts";
 import { ConfigShape } from "../shared/config.ts";
+import { AppError } from "../shared/errors.ts";
 
 export function createTokenHandler(tokenStore: TokenStore, config: ConfigShape) {
     return async (ctx: Context) => {
+        // user_id was set in createKeyCheckMiddleware
         const user_id = ctx.state.user_id as string;
+        
+        const userToken = await tokenStore.get(user_id);
+        if (!userToken) {
+            throw new AppError("User token not found", 404);
+        }
 
-        try {
-            const userToken = await tokenStore.get(user_id);
+        // token is still valid
+        if (Date.now() < userToken.expires_at) {
+            console.log("Retrieved token for user: ", user_id);
+            ctx.response.status = 200;
+            ctx.response.body = { access_token: userToken.access_token }
+            return;
+        }
 
-            // could not retrieve token for user,
-            if (!userToken) {
-                ctx.response.status = 404;
-                ctx.response.body = { error: "User token not found" }
-                return;
+        // token expired, attempt to refresh
+        const response = await getTokenFromRefresh(userToken.refresh_token, config);
+        if ("access_token" in response) {
+            const expires_at = 
+                Date.now() + 
+                (response.expires_in - config.REFRESH_MARGIN) * 1000;
+            const newUserToken: StoredToken = {
+                ...response,
+                user_id,
+                expires_at,
+                refresh_token: response.refresh_token || userToken.refresh_token,
             }
-
-            if (Date.now() < userToken.expires_at) {
-                console.log("Retrieved token for user: ", user_id);
-                ctx.response.status = 200;
-                ctx.response.body = { access_token: userToken.access_token }
-                return;
-            }
-
-            const response = await getTokenFromRefresh(userToken.refresh_token, config);
-
-            if ("access_token" in response) {
-                const expires_at = Date.now() + (response.expires_in - config.REFRESH_MARGIN) * 1000;
-                const newUserToken: StoredToken = {
-                    ...response,
-                    user_id,
-                    expires_at,
-                    refresh_token: response.refresh_token || userToken.refresh_token,
-                }
-                await tokenStore.upsert(newUserToken);
-                console.log("New token from refresh successful: user_id:", user_id);
-                ctx.response.status = 200;
-                ctx.response.body = { access_token: newUserToken.access_token };
-            } else {
-                console.error("Error getting token from code:", response.error_description);
-                ctx.response.status = response.status;
-                ctx.response.body = { error: response.error_description };
-            }
-
-        } catch (_err) {
-                console.error("Could not get get user info: ", user_id);
-                console.error(_err);
-                ctx.response.status = 500;
-                ctx.response.body = { error: "Internal server error" };
+            await tokenStore.upsert(newUserToken);
+            console.log("New token from refresh successful: user_id:", user_id);
+            ctx.response.status = 200;
+            ctx.response.body = { access_token: newUserToken.access_token };
+        } else {
+            console.error("Error getting token from code:", response.error_description);
+            ctx.response.status = response.status;
+            ctx.response.body = { error: response.error_description };
         }
     };
 }
